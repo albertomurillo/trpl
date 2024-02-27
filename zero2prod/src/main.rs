2a1f5c39327e7d098b53ac3681c1f6320f9c00d3
Alberto Murillo <albertomurillosilva@gmail.com>
2024-02-27 15:14:29 -0800

    Add /health_check

--- zero2prod/src/main.rs
+++ zero2prod/src/main.rs
@@ -1,3 +1,5 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = std::net::TcpListener::bind("127.0.0.1:8000")?;
+    zero2prod::run(listener)?.await
 }
